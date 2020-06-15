a9f102403042ce248927b9917736fbaa7f34bc95
quang <quang@DESKTOP-L05F6HO>
2020-06-15 10:15:25 +0700

    Status panel just like an example fỏ status in homepage

---
+++ social-media-desktop-client/src/main/java/edu/hanu/social_media_desktop_client/gui/StatusPanel.java
@@ -0,0 +1,85 @@
+package edu.hanu.social_media_desktop_client.gui;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+
+import edu.hanu.social_media_desktop_client.utils.PlaceHolderTextField;
+
+public class StatusPanel extends JFrame {
+	private JLabel lbName;
+	private JLabel lbTime;
+	private JLabel lbStatus;
+	private JButton btnLike;
+	private JLabel lbCommenterName;
+	private JLabel lbCommentTime;
+	private JLabel lbComment;
+	private PlaceHolderTextField textComment;
+	private JButton btnComment;
+	private JScrollPane jScrollPane;
+
+	public StatusPanel() {
+		super("Status");
+		setSize(350, 380);
+		setLocation(500, 100);
+		setLayout(null);
+		initPanels();
+	}
+
+	private void initPanels() {
+		lbName = new JLabel("Vuong Khanh Linh");
+		lbName.setSize(300, 40);
+		lbName.setLocation(20, 0);
+		add(lbName);
+
+		lbTime = new JLabel("20/11/2020");
+		lbTime.setSize(300, 40);
+		lbTime.setLocation(20, 30);
+		add(lbTime);
+
+		lbStatus = new JLabel("It's awesome dayyyyyyyyyy.");
+		lbStatus.setSize(300, 40);
+		lbStatus.setLocation(20, 50);
+		add(lbStatus);
+		
+		btnLike = new JButton("Like");
+		btnLike.setSize(60, 20);
+		btnLike.setLocation(20, 90);
+		add(btnLike);
+
+		lbCommenterName = new JLabel("Tran Thu Hien");
+		lbCommenterName.setSize(100, 20);
+		lbCommenterName.setLocation(40, 110);
+		add(lbCommenterName);
+		
+		lbCommentTime = new JLabel("22/22/2020");
+		lbCommentTime.setSize(100, 20);
+		lbCommentTime.setLocation(150, 110);
+		add(lbCommentTime);
+		
+		lbComment = new JLabel("Ohh wowwwww");
+		lbComment.setSize(100, 20);
+		lbComment.setLocation(20, 140);
+		add(lbComment);
+		
+		textComment = new PlaceHolderTextField(30);
+		textComment.setPlaceholder("Write some comments about this post!");
+		textComment.setSize(220, 70);
+		textComment.setLocation(10, 180);
+		add(textComment);
+		
+		btnComment = new JButton("Comment");
+		btnComment.setSize(90, 30);
+		btnComment.setLocation(240, 200);
+		add(btnComment);
+		
+	}
+
+	public static void main(String[] args) {
+		StatusPanel statusPanel = new StatusPanel();
+		statusPanel.setVisible(true);
+	}
+
+}
